aedc1231afeb26e1380f2f00792711f6cdde0336
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-06-12 22:52:38 +0300

    🛠️refactor(server): remove console.log statement.

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ const io = initIoServer(httpServer);
 // Start the server
 export const startServer = (port: string | number) => {
   return httpServer.listen(port, () => {
-    // 5173 is the port number
     console.log('Starting server...');
     console.log(`App listening on port ${port}`);
   });
