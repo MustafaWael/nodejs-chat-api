db6f78f76ab0be2045ae4535cc7fa0e086cf119b
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-06-12 23:04:19 +0300

    ⚙️chore(routes) add user router and auth error middleware.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { userRouter } from './routes';
+import { authError } from './middlewares/errors/authErrors';
 
 // Express App Instence
 const app = express();
@@ -7,6 +9,9 @@ const app = express();
 // App Middlewares
 app.use(cors());
 app.use(express.json());
+
+// App Routes
+app.use([userRouter, authError]);
 
 // Export Express App Instence
 export default app;
