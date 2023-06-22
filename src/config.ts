5dc9472690658c72751f9986183dcff5636956aa
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-06-22 05:07:33 +0300

    ⚙️chore(config) add client URL

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,4 @@ export const MONGODB_ATLAS_URI = process.env.MONGODB_ATLAS_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const SENDER_EMAIL = process.env.SENDER_EMAIL;
 export const SENDER_EMAIL_PASSWORD = process.env.SENDER_EMAIL_PASSWORD;
+export const CLIENT_URL = process.env.CLIENT_URL;
