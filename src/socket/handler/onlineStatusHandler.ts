6d95447e603b6f8f414bb90890dea8e38401567b
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 20:47:27 +0300

    Refactor it to send the userId also

--- src/socket/handler/onlineStatusHandler.ts
+++ src/socket/handler/onlineStatusHandler.ts
@@ -3,9 +3,13 @@ import { SocketType, connections } from '../index';
 export const onlineStatusHandler = (socket: SocketType) => {
   const isOnline = connections.has(socket.data?.user?._id.toString());
 
-  socket.broadcast.emit('online', { isOnline });
+  socket.broadcast.emit('online', { isOnline, userId: socket.data?.user?._id });
 
   socket.on('disconnect', () => {
-    socket.broadcast.emit('online', { isOnline: false });
+    // send the id of the user that disconnected and check if he is online or not in the client side
+    socket.broadcast.emit('online', {
+      isOnline: false,
+      userId: socket.data?.user?._id,
+    });
   });
 };
