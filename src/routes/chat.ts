32b58d7c8b3580511b6e60a4c6c61e91bd138492
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 20:40:58 +0300

    Add the /chats & /chat/:id routes

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
-import { createChatHandler } from '../handlers/chat';
+import {
+  createChatHandler,
+  getChatHandler,
+  getChatsHandler,
+} from '../handlers/chat';
 import { auth } from '../middlewares';
 
 const router = Router();
 
 // Create a new chat
 router.post('/chats', auth, createChatHandler);
+
+// Get all chats of the authenticated user
+router.get('/chats', auth, getChatsHandler);
+
+// Get a chat by id
+router.get('/chats/:id', auth, getChatHandler);
+
+// Update a chat by id
+
+// Delete a chat by id
 
 export default router;
