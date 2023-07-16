c843b9dd2fd4d37e975f76dafe27ca14521cb188
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 20:37:08 +0300

    Add getChats & getChat handlers

--- src/handlers/chat.ts
+++ src/handlers/chat.ts
@@ -14,3 +14,31 @@ export const createChatHandler = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+export const getChatsHandler = async (req: Request, res: Response) => {
+  try {
+    const { user } = req;
+
+    // Get all chats of the authenticated user
+    const chats = await chatServices.getChats(user._id);
+
+    res.status(200).json(chats);
+  } catch (error) {
+    console.error('Error getting chats', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const getChatHandler = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    // Get a chat by id
+    const chat = await chatServices.getChat(id);
+
+    res.status(200).json(chat);
+  } catch (error) {
+    console.error('Error getting chat', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
