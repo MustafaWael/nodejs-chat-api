9234d40a662086c6aad24a46eef4efec0db72dd5
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 20:33:09 +0300

    Add getChats & getChat in chat services

--- src/services/chat.ts
+++ src/services/chat.ts
@@ -13,3 +13,30 @@ export const createChat = async (participants: Participants) => {
     throw new Error('Error creating chat');
   }
 };
+
+export const getChats = async (userId: string) => {
+  try {
+    // Get all chats of the authenticated user
+    const chats = await Chat.find({ participants: userId }).populate(
+      'participants',
+      'name email',
+    );
+
+    return chats;
+  } catch (error) {
+    console.error('Error getting chats', error);
+    throw new Error('Error getting chats');
+  }
+};
+
+export const getChat = async (id: string) => {
+  try {
+    // Get a chat by id
+    const chat = await Chat.findById(id).populate('participants', 'name email');
+
+    return chat;
+  } catch (error) {
+    console.error('Error getting chat', error);
+    throw new Error('Error getting chat');
+  }
+};
