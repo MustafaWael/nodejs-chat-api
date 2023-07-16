fc738aa8369ed982e784a50eeee04a9479e79552
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 20:35:42 +0300

    Refactor the removeSensitiveData to make it return the _id of the user also

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,14 +1,16 @@
 import { IUser } from '../database/models/user';
 
 interface UserWithoutSensitiveData {
+  _id: string;
   name: string;
   email: string;
 }
 
 export const removeSensitiveData = (user: IUser): UserWithoutSensitiveData => {
-  const { name, email } = user;
+  const { name, email, _id } = user;
 
   return {
+    _id,
     name,
     email,
   };
