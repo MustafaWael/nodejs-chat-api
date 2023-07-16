ef75d692b73cca76c6c2f84d86a4b9b3ed5914ae
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-07-16 20:42:39 +0300

    Add the message router to express app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { chatRouter, userRouter } from './routes';
+import { chatRouter, messageRouter, userRouter } from './routes';
 import { authError } from './middlewares/errors/authErrors';
 
 // Express App Instence
@@ -13,6 +13,7 @@ app.use(express.json());
 // App Routes
 app.use([userRouter, authError]);
 app.use(chatRouter);
+app.use(messageRouter);
 
 // Export Express App Instence
 export default app;
